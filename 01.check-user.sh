5cbdf6150cc16d22a7983e44825c61606deb9524
root <root@infra01.dual.edu>
2019-07-22 11:12:41 +0200

    01.check-user.sh

--- 01.check-user.sh
+++ 01.check-user.sh
@@ -4,21 +4,24 @@ user=pty
 uid=100
 gid=1000 
 
-
+# User existence
 if getent passwd $user > /dev/null 2>&1; then
     echo "yes the user exists" && points_1=10
 else
     echo "No, the user does not exist"
 fi
 
-
+# Correct uid
 if [ $uid==`getent passwd $user | awk -F ":" '{print $3}'` ] ; then
     echo "correct uid" && points=10
 else
     echo "wrong uid"
 fi
 
+# Correct gid
+if [ $gid==`getent passwd $user | awk -F ":" '{print $4}'` ] ; then
+    echo "correct gid" && points=10
+else
+    echo "wrong gid"
+fi
 
-
-#getent passwd pety | awk -F ":" '{print $3}' -> uid
-#getent passwd pety | awk -F ":" '{print $4}' -> gid
